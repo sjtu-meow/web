d62c9570e148ef7cc146c3aa3217882bebffea2c
TerCZ <t.chuzhe@qq.com>
2017-07-06 16:25:43 +0800

    Start building article editing page.

---
+++ src/main/web/public/App.vue
@@ -0,0 +1,23 @@
+<template>
+<div id="app">
+  <div class="container">
+    <h1>summernote webpack test</h1>
+    <div class="summernote"></div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  created() {
+    $(function() {
+      $('.summernote').summernote({
+        height: 300, // set editor height
+        minHeight: null, // set minimum height of editor
+        maxHeight: null, // set maximum height of editor
+        focus: true // set focus to editable area after initializing summernote
+      });
+    });
+  }
+}
+</script>
