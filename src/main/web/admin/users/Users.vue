fd7e298729ff74f444838297042d9d899baaf95a
TerCZ <t.chuzhe@qq.com>
2017-07-12 09:45:45 +0800

    Add bio field for new user creation.

--- src/main/web/admin/users/Users.vue
+++ src/main/web/admin/users/Users.vue
@@ -27,7 +27,7 @@
       </table>
     </div>
     <div class="text-center">
-      <pagination :pagination="pagination" @changePage="fetchUsers"/>
+      <pagination :pagination="pagination" @changePage="fetchUsers" />
     </div>
   </section>
 
@@ -44,22 +44,27 @@
             <form class="col-md-8">
               <div class="form-group">
                 <label class="control-label" for="new-user-phone">手机</label>
-                <input type="number" class="form-control" id="new-user-phone" v-model="newUserPhone" required/>
-                <span class="help-block"></span>
-              </div>
-              <div class="form-group">
-                <label class="control-label " for="new-user-nickname">昵称</label>
-                <input type="text" class="form-control" id="new-user-nickname" v-model="newUserNickname" required>
+                <input type="number" class="form-control" id="new-user-phone" v-model="newUser.phone" required/>
                 <span class="help-block"></span>
               </div>
               <div class="form-group">
                 <label class="control-label " for="new-user-password">密码</label>
-                <input type="password" class="form-control" id="new-user-password" v-model="newUserPassword" required>
+                <input type="password" class="form-control" id="new-user-password" v-model="newUser.password" required>
+                <span class="help-block"></span>
+              </div>
+              <div class="form-group">
+                <label class="control-label " for="new-user-nickname">昵称</label><span class="text-muted">（选填）</span>
+                <input type="text" class="form-control" id="new-user-nickname" v-model="newUser.nickname" required>
+                <span class="help-block"></span>
+              </div>
+              <div class="form-group">
+                <label class="control-label " for="new-user-bio">签名</label><span class="text-muted">（选填）</span>
+                <input type="text" class="form-control" id="new-user-bio" v-model="newUser.bio" required>
                 <span class="help-block"></span>
               </div>
               <div class="checkbox">
                 <label>
-                  <input type="checkbox" v-model="newUserisAdmin"/> 管理员权限
+                  <input type="checkbox" v-model="newUser.isAdmin"/> 管理员权限
                 </label>
               </div>
             </form>
@@ -68,7 +73,7 @@
                 <div class="form-group">
                   <label class="control-label" for="new-user-avatar">默认头像</label><span class="text-muted">（不让你改）</span>
                   <a class="thumbnail">
-                    <img :src="newUserAvatarUrl">
+                    <img :src="newUser.avatarUrl">
                   </a>
                 </div>
               </form>
@@ -179,11 +184,14 @@ export default {
         totalPages: 1
       },
       pageSize: 2,
-      newUserNickname: '',
-      newUserPassword: '',
-      newUserPhone: '',
-      newUserAvatarUrl: 'http://osg5c99b1.bkt.clouddn.com/defaultAvatar.png',
-      newUserisAdmin: false,
+      newUser: {
+        phone: '',
+        password: '',
+        nickname: '',
+        bio: '',
+        avatarUrl: 'http://osg5c99b1.bkt.clouddn.com/defaultAvatar.png',
+        isAdmin: false
+      },
       userToDelete: {
         id: 1,
         phone: '13512341234',
@@ -235,11 +243,12 @@ export default {
     addUser() {
       //TODO: change url
       this.$http.post('http://106.14.156.19/api/admin/users', {
-        phone: this.newUserPhone,
-        password: this.newUserPassword,
-        nickname: this.newUserNickname,
-        admin: this.newUserisAdmin,
-        avatar: this.newUserAvatarUrl
+        phone: this.newUser.phone,
+        password: this.newUser.password,
+        nickname: this.newUser.nickname,
+        bio: this.newUser.bio,
+        admin: this.newUser.isAdmin,
+        avatar: this.newUser.avatarUrl
       }).then(function(response) {
         this.fetchUsers(this.pagination.totalPages - 1);
         $('#add-user-modal').modal('hide');
