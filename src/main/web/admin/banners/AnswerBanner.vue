d5f2e15e9360a37018fb6aa90bba844c4cf5a9dd
TerCZ <t.chuzhe@qq.com>
2017-07-13 17:13:22 +0800

    Add label and organize button in group.

--- src/main/web/admin/banners/AnswerBanner.vue
+++ src/main/web/admin/banners/AnswerBanner.vue
@@ -11,24 +11,33 @@
   <div class="panel-body">
     <div class="row">
       <div class="col-md-4">
+        <label>吧呢封面</label><span class="text-muted">（点击修改）</span>
         <a class="thumbnail">
             <img :src="banner.image">
           </a>
       </div>
       <div class="col-md-8">
+        <label>回答预览</label>
         <p v-for="line in plainContent.substring(0, contentPreviewLength).split('\n')">{{line}}</p>
-        <p><a @click="expandContent">查看详情</a></p>
         <p>
-          <div class="btn-group" role="group">
-            <button type="button" class="btn btn-default btn-sm" @click="moveUp">
-              <span class="glyphicon glyphicon-arrow-up"/>
-            </button>
-            <button type="button" class="btn btn-default btn-sm" @click="deleteBanner">
-              <span class="glyphicon glyphicon-remove"/>
-            </button>
-            <button type="button" class="btn btn-default btn-sm" @click="moveDown">
-              <span class="glyphicon glyphicon-arrow-down"/>
-            </button>
+          <div class="btn-toolbar">
+            <div class="btn-group" role="group">
+              <button type="button" class="btn btn-default btn-sm" @click="expandContent">
+                查看详情
+              </button>
+            </div>
+
+            <div class="btn-group" role="group">
+              <button type="button" class="btn btn-default btn-sm" @click="moveUp">
+                <span class="glyphicon glyphicon-arrow-up"/>
+              </button>
+              <button type="button" class="btn btn-default btn-sm" @click="deleteBanner">
+                <span class="glyphicon glyphicon-remove"/>
+              </button>
+              <button type="button" class="btn btn-default btn-sm" @click="moveDown">
+                <span class="glyphicon glyphicon-arrow-down"/>
+              </button>
+            </div>
           </div>
         </p>
       </div>
