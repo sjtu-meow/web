1374e13da65265e2f51f0daba3de3a780e2790f2
gousaiyang <gousaiyang@163.com>
2017-09-03 21:31:23 +0800

    Home page contents(partial)

--- src/main/java/me/sjtumeow/meow/model/result/BaseSummaryResult.java
+++ src/main/java/me/sjtumeow/meow/model/result/BaseSummaryResult.java
@@ -1,7 +1,17 @@
 package me.sjtumeow.meow.model.result;
 
 public class BaseSummaryResult {
+
+    protected Long id;
     protected String createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getCreateTime() {
         return createTime;
