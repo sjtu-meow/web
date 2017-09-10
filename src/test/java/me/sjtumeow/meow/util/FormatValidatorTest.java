e2e58718ea5da08136f459bde3846b492be27ef2
Wuwei Lin <vincentl13x@gmail.com>
2017-09-10 16:58:31 +0800

    Add test stub

---
+++ src/test/java/me/sjtumeow/meow/util/FormatValidatorTest.java
@@ -0,0 +1,36 @@
+package me.sjtumeow.meow.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FormatValidatorTest {
+    @Test
+    public void checkNonNegativeInt() throws Exception {
+    }
+
+    @Test
+    public void checkPositiveInt() throws Exception {
+    }
+
+    @Test
+    public void checkPhone() throws Exception {
+    }
+
+    @Test
+    public void checkPassword() throws Exception {
+    }
+
+    @Test
+    public void checkSmsCode() throws Exception {
+    }
+
+    @Test
+    public void checkMainItemType() throws Exception {
+    }
+
+    @Test
+    public void checkItemType() throws Exception {
+    }
+
+}
---
+++ src/test/java/me/sjtumeow/meow/util/StringUtilTest.java
@@ -0,0 +1,32 @@
+package me.sjtumeow.meow.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringUtilTest {
+    @Test
+    public void replaceNull() throws Exception {
+    }
+
+    @Test
+    public void filterRichText() throws Exception {
+    }
+
+    @Test
+    public void extractHTMLSummary() throws Exception {
+    }
+
+    @Test
+    public void wrapLikeSubstr() throws Exception {
+    }
+
+    @Test
+    public void tryStringToPosLong() throws Exception {
+    }
+
+    @Test
+    public void parseReportStatus() throws Exception {
+    }
+
+}
