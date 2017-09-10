ec287e11aac9faed6616a36905077fa2d18f1924
Wuwei Lin <vincentl13x@gmail.com>
2017-09-10 20:16:13 +0800

    Add token test

---
+++ src/test/java/me/sjtumeow/meow/authorization/manager/impl/RedisTokenManagerTest.java
@@ -0,0 +1,38 @@
+package me.sjtumeow.meow.authorization.manager.impl;
+
+import me.sjtumeow.meow.authorization.manager.TokenManager;
+import me.sjtumeow.meow.authorization.model.TokenModel;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import sun.tools.jstat.Token;
+
+import static org.junit.Assert.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class RedisTokenManagerTest {
+    @Autowired TokenManager tokenManager;
+
+    @Test
+    public void checkToken() throws Exception {
+        Long userId = 1L;
+        TokenModel token = tokenManager.createToken(userId);
+        assertTrue(tokenManager.checkToken(token));
+        tokenManager.deleteToken(userId);
+        assertFalse(tokenManager.checkToken(token));
+    }
+
+    @Test
+    public void getToken() throws Exception {
+        String userId = "123";
+        String tokenString = "tokenString";
+        String auth = userId + '_' + tokenString;
+        TokenModel token = tokenManager.getToken(auth);
+        assertEquals(Long.valueOf(userId), Long.valueOf(token.getUserId()));
+        assertEquals(tokenString, token.getToken());
+    }
+
+}
